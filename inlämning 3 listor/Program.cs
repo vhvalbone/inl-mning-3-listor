cddeb32bfe888953ca1f55d29506514146a8ce93
vhvalbone <albin06svensson@gmail.com>
2024-02-09 12:37:20 +0100

    Add project files.

---
+++ inlämning 3 listor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> landStorlekar = new Dictionary<string, int>()
+        {
+            {"Sverige", 450},
+            {"Norge", 385},
+            {"Danmark", 43},
+            {"Finland", 338}
+        };
+
+        Console.Write("Skriv namnet på ett av följande länder: Sverige, Norge, Danmark, Finland: ");
+        string inputLand = Console.ReadLine();
+
+        if (landStorlekar.ContainsKey(inputLand))
+        {
+            int storlek = landStorlekar[inputLand];
+            Console.WriteLine($"Storleken på {inputLand} är {storlek} hundratusentals kvadratkilometer.");
+        }
+        else
+        {
+            Console.WriteLine("Landet du angav finns inte i listan.");
+        }
+    }
+}
